7b8e26cfc4d9c7a8930b34cd1771dafdb087facc
GanerCodes <ganer@ganer.xyz>
2023-03-31 19:11:46 -0500

    xd

---
+++ server.py
@@ -0,0 +1,16 @@
+# from flask import Flask, render_template, send_from_directory, redirect, url_for
+import sqlitedict
+from flask import *
+database = SqliteDict("database.sqlite", tablename="product", autocommit=True)
+
+app = Flask(__name__)
+
+app.route('/')(lambda: redirect(url_for('static', filename='index.html')))
+
+@app.route('/api')
+def api():
+    data = {
+        "key1": "value1",
+        "key2": "value2"
+    }
+    return jsonify(data)
