a70ed3efd82b566954a3fdec7db80ddfa5dd4b8b
Rithyka Heng <rkheng@uark.edu>
2023-04-01 01:37:51 -0400

    unlaze

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ from database import *
 class Reply:
     error = lambda: (jsonify({'error': 'L+ratio+you fell off'}), 500)
     ok = lambda: Response(status=200)
-    def __getattr__(self, name):
-        return getattr(Reply, name)()
-Reply = Reply()
+    # def __getattr__(self, name):
+        # return getattr(Reply, name)()
+# Reply = Reply()
 
 app = Flask(__name__)
 
@@ -18,18 +18,18 @@ def getPanelData():
     panel = get_panel(data['id'])
     if panel:
         return jsonify(panel)
-    return Reply.error
+    return Reply.error()
 
 @app.route('/addPanel', methods=['POST'])
 def addPanel():
     data = request.get_json()
     if get_panel(data['id']):
-        return Reply.error
+        return Reply.error()
     set_panel(data)
-    return Reply.ok
+    return Reply.ok()
 
 @app.route('/setPanelData', methods=['POST'])
 def setPanelData():
     data = request.get_json()
     merge_panel(data)
-    return Reply.ok
+    return Reply.ok()
