4a4c313ecd5a31697063cc94ae313637c8583f32
GanerCodes <ganer@ganer.xyz>
2023-03-31 22:17:40 -0500

    xd

--- server.py
+++ server.py
@@ -1,22 +1,30 @@
-# from flask import Flask, render_template, send_from_directory, redirect, url_for
-from sqlitedict import SqliteDict
 from flask import *
-from Panel import Panel
-database = SqliteDict("panels.sqlite", tablename="panels", autocommit=True)
+from database import *
 
-def add_panel(panel: Panel):
-    
-    print('a' in database)
+class Response:
+    error = jsonify({'error': 'L+ratio+you fell off'}), 500
+    ok = Response(status=200)
 
-exit()
 app = Flask(__name__)
 
 app.route('/')(lambda: redirect(url_for('static', filename='index.html')))
 
-@app.route('/api')
-def api():
-    data = {
-        "key1": "value1",
-        "key2": "value2"
-    }
-    return jsonify(data)
+@app.route('/getPanelData', methods=['POST'])
+def getPanelData():
+    data = request.get_json()
+    panel = get_panel(data['id'])
+    if panel:
+        return jsonify(panel)
+    return Response.error
+
+@app.route('/addPanel', methods=['POST'])
+def getPanelData():
+    data = request.get_json()
+    set_panel(data)
+    return Response.ok
+
+@app.route('/setPanelData', methods=['POST'])
+def getPanelData():
+    data = request.get_json()
+    merge_panel(data)
+    return Response.ok
