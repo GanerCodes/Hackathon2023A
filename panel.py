593dffc8327abc918d6171dec1bd59de3d12bbbc
GanerCodes <ganer@ganer.xyz>
2023-04-01 15:29:20 -0500

    xd

--- panel.py
+++ panel.py
@@ -17,7 +17,6 @@ def fit_data(obj, template):
                 res[n] = None
     return res
 
-
 Panel_template = {
     "id": str,
     "battery": {
@@ -46,7 +45,11 @@ def create_schedule(curve, panel):
                           dt=1/60)
 
     extremes = optimizer.compute_extremes(
-        optimizer.max_p_integral, optimizer.Pm, True)
+        optimizer.max_p_integral,
+        optimizer.Pm,
+        True)
+    
+    plt.gcf().set_size_inches(9, 5)
     plt.plot(optimizer.max_p_integral)
     plt.scatter(*zip(*extremes))
     optimizer.merge_tops()
@@ -54,7 +57,7 @@ def create_schedule(curve, panel):
     plt.plot(*zip(*enumerate(optimizer.Pa)))
 
     img_path = f"./static/panel_images/{Id}.png"
-    plt.savefig(img_path)
+    plt.savefig(img_path, dpi=200)
 
     return {
         "state_schedule": optimizer.Pa,
