af64b9bc728786fe25c0f6f58c4278ba5a7abb80
Rithyka Heng <rkheng@uark.edu>
2023-04-01 16:13:35 -0400

    thing

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,3 +1,47 @@
 var pNameElement,
+	pLocationElement,
 	pChargeElement,
-	pb
+	pChargingRateElement,
+	pDechargingRateElement;
+
+function submitPanel() {
+	//check if good
+	//
+
+	//actually add panel
+	let id = pNameElement.value,
+		currentCharge = Number(pChargeElement.value),
+		chargingRate = Number(pChargingRateElement.value),
+		dischargingRate = Number(pDechargingRateElement.value);
+	if(panelIds.indexOf(id) < 0) {
+		//doesn't yet exist, add panel
+		addPanel(
+			new Panel(
+				id,
+				new Battery(
+					BatteryStates.CHARGING,
+					currentCharge,
+					chargingRate,
+					dischargingRate
+				)
+			)
+		);
+	}
+	else {
+		setPanelData(
+			id,
+			new ChargeRates(
+				chargingRate,
+				dischargingRate
+			)
+		);
+	}
+}
+
+window.addEventListener("load", () => {
+	pNameElement = document.getElementById("id");
+	pLocationElement = document.getElementById("location");
+	pChargeElement = document.getElementById("currcharge");
+	pChargingRateElement = document.getElementById("chargerate");
+	pDechargingRateElement = document.getElementById("dischargerate");
+});
